00e8fddb1de725dcbfa33d683c41017c0b6556b3
Chrissy Semens <chrissysemens@gmail.com>
2019-03-03 12:01:34 +0000

    Added functionality to create playlists for a user

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from '../core/data.service';
+import { Profile } from '../types/profile';
+import { PathLocationStrategy } from '@angular/common';
+import { Observable } from 'rxjs';
 
 const baseUrl = 'https://api.spotify.com';
+
+const routes = {
+    me: () => `/v1/me`,
+  };
 
 @Injectable()
 export class ProfileService extends DataService<any>{
     
+    profile: Profile;
+
     constructor(http: HttpClient) {
         super(http, baseUrl);
+    }
+
+    getMyProfile(): Observable<any>{
+        return this.query(routes.me());
     }
 }
