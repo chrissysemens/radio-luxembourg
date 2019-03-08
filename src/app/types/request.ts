9edc78155ae8234b952d740e17003c170fc8db66
Chrissy Semens <chrissysemens@gmail.com>
2019-03-08 22:23:22 +0000

    Removed some profile data from request queue

--- src/app/types/request.ts
+++ src/app/types/request.ts
@@ -5,12 +5,12 @@ export class Request {
     request_time: Date
     track_start: number
     track: Track
-    profile: Profile
+    requestor: string
 
     constructor(track: Track, track_start: number, profile: Profile){
         this.track = track;
         this.request_time = new Date();
         this.track_start = track_start;
-        this.profile = profile;
+        this.requestor = profile.display_name;
     }
 }
