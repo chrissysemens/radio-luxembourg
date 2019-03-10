5f7e14971ed4f1b58d1bb5bc42cb289c58965656
Chrissy Semens <chrissysemens@gmail.com>
2019-03-10 12:44:24 +0000

    Now only using one Playlist...

--- src/app/channel/channel.component.ts
+++ src/app/channel/channel.component.ts
@@ -8,19 +8,22 @@ import { UserPlaylistService } from '../playlist/user-playlist.service';
 import { Playlist } from '../types/playlist';
 import { Session } from '../types/sesssion';
 import { SessionService } from '../session/session.service';
+import { MyPlaylistService } from '../playlist/my-playlist.service';
 
 @Component({
   templateUrl: './channel.component.html',
   styleUrls: ['./channel.component.scss'],
-  providers: [ProfileService, UserPlaylistService]
+  providers: [MyPlaylistService, ProfileService, UserPlaylistService]
 })
 
 export class ChannelComponent implements OnInit {
 
   channel: Channel;
+  playlistId: string;
 
   constructor(
     private route: ActivatedRoute,
+    private myPlaylistService: MyPlaylistService,
     private profileService: ProfileService,
     private userPlaylistService: UserPlaylistService,
     private sessionService: SessionService
@@ -30,18 +33,34 @@ export class ChannelComponent implements OnInit {
     const channelId = this.route.snapshot.paramMap.get("id");
     const playlistReq = new CreatePlaylistRequest('RadioLux', true, false, 'You control the jams');
 
+
     this.profileService.getMyProfile()
       .subscribe( 
         (profile: Profile) => {
           let user = profile;
 
-          this.userPlaylistService.createRadioPlaylist(user.id, playlistReq)
-            .subscribe(
-              (playlist: Playlist) => {
+          this.myPlaylistService.getMyPlaylists()
+            .subscribe((data: any) => {
 
-              const session = new Session(user, channelId, playlist.id);
-              this.sessionService.createSession(session);
-          });
-      });
-  }
+                const playlists = data.items;
+                playlists.forEach((playlist: Playlist) => {
+                  if(playlist.name === 'RadioLux') {
+                    this.playlistId = playlist.id;
+                  }
+                });
+
+                if(this.playlistId){
+                  const session = new Session(user, channelId, this.playlistId);
+                  this.sessionService.createSession(session);
+                } else {
+                    this.userPlaylistService.createRadioPlaylist(user.id, playlistReq)
+                      .subscribe(
+                        (playlist: Playlist) => {
+                          const session = new Session(user, channelId, playlist.id);
+                          this.sessionService.createSession(session);
+                      }).unsubscribe();
+                }
+            }).unsubscribe();
+      }).unsubscribe();
+    }
 }
